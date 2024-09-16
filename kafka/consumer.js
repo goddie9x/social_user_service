e5ef16c4fc20c6b2b2d1ea7515ee9bcd85e8ca5c
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-16 18:48:30 +0700

    refactor: migrate from kafka-node to kafkajs

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -1,11 +1,9 @@
 const userService = require('../services/userService');
-const { kafkaClient } = require('../utils/kafka/producer');
 const { USER_TOPIC } = require('../constants/kafkaTopic');
 const { activeServiceConsumer } = require('../utils/kafka/consumer');
 
 const activeUserServiceConsumer = () => {
     activeServiceConsumer({
-        kafkaClient,
         serviceInstance: userService,
         topic: USER_TOPIC.REQUEST
     });
